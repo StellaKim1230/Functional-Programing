7fcb333f7cd18ce26ceafc4b42e81ccd3753ed8b
Stellakim1230 <stellakim1230@gmail.com>
2018-12-23 16:55:59 +0900

    modify README.md, add pipe.js file.

--- example/pipe.js
+++ example/pipe.js
@@ -5,6 +5,12 @@ const getMul = (a) => a * 2
 function pipe(...fns) {
   const fs = fns
   return (a) => fs.reduce((v, f) => f(v), a)
+  // reduce ((v, f) => f(v), a)
+  // reduce ((계산된 누적값, 배열에서 현재값) => 결과값, 계산된 누적값이 없을경우 초기값)
+  // p(3) 1. 3, 6
+  // p(3) 2. 6, 5
+  // p(3) 3. 5, 10
+  // 10으로 리턴
 }
 
 const p = pipe(getSum, getMinus, getMul)
@@ -12,3 +18,7 @@ p(3)
 
 const arr = [ 1, 2, 3 ]
 arr.reduce((v, v1) => v + v1)
+
+// 1, 1
+// 1, 3
+// 3, 3 => 6이 리턴
